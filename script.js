2947363be67a820b8b12bcd23343584a14635c95
Blackman <DBlackman@int.co.chenango.ny.us>
2024-06-11 11:01:17 -0400

    Add operator button functionality

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const buttons = document.querySelectorAll(".btn")
+const numButtons = document.querySelectorAll(".num-btn")
+const operatorBtn = document.querySelectorAll(".op-btn")
+const equalsBtn = document.getElementById("equals")
+const clearBtn = document.getElementById(".clear")
+const decimalBtn = document.getElementById("decimal")
 const display = document.getElementById("display")
 const disStatus = document.getElementById("display-status")
 
@@ -9,13 +13,48 @@ disStatus.textContent = '-'
 
 
 // add event listeners for buttons
-buttons.forEach((btn) => {
+
+// number buttons
+numButtons.forEach((btn) => {
     btn.addEventListener("click", () => {
-        return btn.textContent
+        showValue(btn)
     })
 })
 
+// operator buttons
+operatorBtn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        operatorValue(btn)
+    })
+})
+
+// clear button
+
+// decimal button
+
+// equals button
+
+
+
 // show value in display
-const showValue = () => {
-    
+const showValue = (value) => {
+    if (display.textContent == '0'){
+        display.textContent = value.textContent
+    } else if (['+', '-', '*', '/'].includes(display.textContent)) {
+        disStatus.textContent += ' ' + display.textContent
+        display.textContent = value.textContent
+    } else {
+        display.textContent += value.textContent
+    }
+}
+
+// operator button - show operator and add display value to status
+const operatorValue = (value) => {
+    if (disStatus.textContent == '-'){
+        disStatus.textContent = display.textContent
+    } else {
+        disStatus.textContent += ' ' + display.textContent
+    }
+
+    display.textContent = value.textContent
 }
